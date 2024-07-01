389a14ac9a6db94f3a7cb2ad6723953e54f0d377
Iri O <oparina.iri@gmail.com>
2024-07-01 19:39:48 +0500

    Updated settings

--- config/settings.py
+++ config/settings.py
@@ -48,11 +48,14 @@ DJANGO_APPS = [
 
     'rest_framework',
     'rest_framework_simplejwt',
+    'rest_framework_simplejwt.token_blacklist',
     'solo',
 ]
 
 PROJECT_APPS = [
     'users',
+    'notifications',
+    'characters',
 ]
 
 INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS
@@ -126,9 +129,25 @@ REST_FRAMEWORK = {
 }
 
 # JWT
+
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
+    'ROTATE_REFRESH_TOKENS': False,
+    'BLACKLIST_AFTER_ROTATION': True,
+    'ALGORITHM': 'HS256',
+    'SIGNING_KEY': SECRET_KEY,
+    'VERIFYING_KEY': None,
+    'AUTH_HEADER_TYPES': ('Bearer',),
+    'AUTH_HEADER_NAME': 'HTTP_AUTHORIZATION',
+    'USER_ID_FIELD': 'id',
+    'USER_ID_CLAIM': 'user_id',
+    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+    'TOKEN_TYPE_CLAIM': 'token_type',
+    'JTI_CLAIM': 'jti',
+    'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
+    'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
+    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=7),
 }
 
 # Password validation
@@ -195,3 +214,10 @@ EMAIL_PORT = os.environ.get(
     'EMAIL_PORT', 587
 )
 EMAIL_USE_TLS = True
+
+
+# fixtures
+
+FIXTURE_DIRS = (
+    os.path.join(BASE_DIR, 'apps', 'notifications', 'tests', 'fixtures'),
+)
