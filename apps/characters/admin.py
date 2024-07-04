edf4cdd2551c1767206d7672eaa459692675e7d8
Iri O <oparina.iri@gmail.com>
2024-07-04 19:37:17 +0500

    Updated CharacterAdmin

--- apps/characters/admin.py
+++ apps/characters/admin.py
@@ -8,7 +8,28 @@ from characters.models import (
 
 @admin.register(Character)
 class CharacterAdmin(admin.ModelAdmin):
-    pass
+    list_display = [
+        'name',
+        'level',
+        'is_available',
+    ]
+    list_editable = [
+        'is_available',
+    ]
+    list_display_links = [
+        'level',
+    ]
+    list_filter = [
+        'level',
+    ]
+
+    actions = ['make_available', 'make_unavailable']
+
+    def make_available(self, request, queryset):
+        queryset.update(is_available=True)
+
+    def make_unavailable(self, request, queryset):
+        queryset.update(is_available=False)
 
 
 @admin.register(CharactersAPIKey)
